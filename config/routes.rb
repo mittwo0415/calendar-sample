6612757b91fd89482b006af77e860723b5372814
EC2 Default User <ec2-user@ip-172-31-6-103.ap-northeast-1.compute.internal>
2024-10-09 13:28:48 +0000

    レスポンシブ対応など

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'events/index'
   root to: "events#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/events', to: 'events#index', defaults: { format: 'json' } 
 end
